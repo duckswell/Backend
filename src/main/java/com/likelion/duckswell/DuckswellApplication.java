abc202db85f03b19b02b0331c6b4994f23ed3a14
leewatertrue <leewatertrue@naver.com>
2026-08-08 18:24:36 +0900

    start: Start New Project

---
+++ src/main/java/com/likelion/duckswell/DuckswellApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.duckswell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckswellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckswellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/duckswell/DuckswellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.duckswell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckswellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
