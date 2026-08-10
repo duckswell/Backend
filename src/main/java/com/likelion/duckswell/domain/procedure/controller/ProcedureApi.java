d9e8b1eb56b7c5197371da181c96bf5c5c24e2de
naeuun <1210kne@naver.com>
2026-08-10 22:31:11 +0900

    docs: 응답 상태 추가

--- src/main/java/com/likelion/duckswell/domain/procedure/controller/ProcedureApi.java
+++ src/main/java/com/likelion/duckswell/domain/procedure/controller/ProcedureApi.java
@@ -4,6 +4,7 @@ import com.likelion.duckswell.domain.procedure.dto.ProcedureRegisterRequest;
 import com.likelion.duckswell.domain.procedure.dto.ProcedureResponse;
 import com.likelion.duckswell.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
@@ -19,5 +20,9 @@ public interface ProcedureApi {
                     함께 등록하는 것을 전제로 합니다.
                     """
     )
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "시술 정보 등록 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "요청 값 검증 실패 (필수값 누락, 시술 부위 미지정 등)")
+    })
     ResponseEntity<ApiResponse<List<ProcedureResponse>>> registerProcedures(ProcedureRegisterRequest request);
 }
